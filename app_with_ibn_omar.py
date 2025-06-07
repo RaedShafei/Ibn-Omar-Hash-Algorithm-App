381aa4d1467a450d0f3266a681f55655acf74730
RaedShafei <69829680+RaedShafei@users.noreply.github.com>
2025-06-07 13:56:34 +0300

    Update app_with_ibn_omar.py

--- app_with_ibn_omar.py
+++ app_with_ibn_omar.py
@@ -25,15 +25,12 @@ def reverse(seq, start, stop):
         seq[i], seq[j] = seq[j], seq[i]
     return ''.join(seq)
 
-def sanitize_input(text):
-    return text.replace('\n', '\\n').replace('\r', '').strip()
-
 def ibn_omar_hash(i):
     ti = time.time()
-    isize = len(i)
 
     i = map(bin, bytearray(i, 'utf-8'))
-    i = ''.join(i).replace('0b', '')
+    i = ''.join(i)
+    i = i.replace('0b', '')
 
     A = '1100010001111001001110110001010001100101001011000101100110010011101100010111111001010010'
     B = '11001000111110010011101100100100011001010001110010011101100011001011001010010'
@@ -46,10 +43,13 @@ def ibn_omar_hash(i):
 
     b = i + A + i + B + i + C + i + D + i + E + i + F + i + G + i + H + i
     block = b + b + '1'
+
     bSize = len(block)
+    paddingValue = (2048 % bSize)
+    pv = (bSize % 2048)
 
     if bSize < 2048:
-        padL = ''.ljust((2048 % bSize), '0')
+        padL = ''.ljust(paddingValue, '0')
         blocks = block + padL
     elif bSize > 2048:
         pv = ((bSize // 2048) + 1) * 2048 - bSize
@@ -74,82 +74,69 @@ def ibn_omar_hash(i):
     bL = int(bL)
     bL = '{0:b}'.format(bL)
 
-    bLDec = sum([int(d) for d in bL])
-    bRDec = sum([int(d) for d in bR])
-    iDecimal = sum([int(d) for d in blocks])
+    bLDec = sum([int(d) * (2 ** idx) for idx, d in enumerate(reversed(bL))])
+    bRDec = sum([int(d) * (2 ** idx) for idx, d in enumerate(reversed(bR))])
 
-    bLDecF = bRDec * bLDec
-    bRDecF = bLDec * bRDec
+    iDecimal = sum([int(d) * (2 ** idx) for idx, d in enumerate(reversed(blocks))])
+
+    factorL = sum([int(d) for d in str(bLDec)])
+    factorR = sum([int(d) for d in str(bRDec)])
+
+    bLDecF = factorR * bLDec
+    bRDecF = factorL * bRDec
 
     bLDecSTR = str(bLDecF)
     bRDecSTR = str(bRDecF)
 
-    try:
-        bLDec = ((bLDecF - int(lShift(bRDecSTR, 1270))) +
-                 (bLDecF - int(rShift(bRDecSTR, 309))) +
-                 (bLDecF - int(reverse(bRDecSTR, -31, 286)))) ** 3
-        bRDec = ((bRDecF - int(lShift(bLDecSTR, 1469))) +
-                 (bRDecF - int(rShift(bLDecSTR, 309))) +
-                 (bRDecF - int(reverse(bLDecSTR, 46, 199)))) ** 5
-    except:
-        return "HASH_ERROR"
+    bLDec = ((bLDecF - int(lShift(bRDecSTR, 1270))) + (bLDecF - int(rShift(bRDecSTR, 309))) + (
+            bLDecF - int(reverse(bRDecSTR, -31, 286)))) ** 3
+
+    bRDec = ((bRDecF - int(lShift(bLDecSTR, 1469))) + (bRDecF - int(rShift(bLDecSTR, 309))) + (
+            bRDecF - int(reverse(bLDecSTR, 46, 199)))) ** 5
 
     bLDec = abs(int(bLDec))
     bRDec = abs(int(bRDec))
 
     iLen = len(i)
-    try:
-        Yl = abs(math.log(bLDec, iLen))
-        Yr = abs(math.log(bRDec, iLen))
-    except:
-        return "HASH_ERROR"
 
-    primes = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149,
-              151, 157, 163, 167, 173, 179, 181, 191, 193, 197,
-              199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
-              263, 269, 271, 277, 281, 283, 293, 307, 311, 313]
+    Yl = abs(math.log(bLDec, iLen))
+    Yr = abs(math.log(bRDec, iLen))
+
+    primes = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149]
 
     for e in primes:
         eL = int(math.ceil((e * Yr) / iLen))
-        bLDec = (eL * bLDec) + eL
-
-    for e in primes:
         eR = int(math.ceil((e * Yl) / iLen))
+        bLDec = (eL * bLDec) + eL
         bRDec = (eR * bRDec) + eR
 
-    try:
-        Y = int(math.floor(Yl * Yr))
-        bDec = (bRDec + bLDec) / Y
-        bDec = int(lShift(str(bDec), Y))
-        bBin = bin(bDec).replace('0b', '')
-        pad_length = ((len(bBin) // 1024) + 1) * 1024 - len(bBin)
-        bBin = bBin + ''.ljust(pad_length, '0')
-        bBin = reverse(bBin, -306, -604)
-        bBin = lShift(bBin, 1024)
-        bBin = reverse(bBin, -910, -295)
-        finalBlock = bBin[:1024]
-        final = hex(int(finalBlock, 2)).replace('L', '')
-        return final
-    except:
-        return "HASH_ERROR"
+    Y = int(math.floor(Yl * Yr))
+    bDec = (bRDec + bLDec) / Y
+    bDec = lShift(str(int(bDec)), Y)
+
+    bBin = bin(int(bDec)).replace('0b', '')
+
+    if len(bBin) > 1024:
+        bBin = bBin.ljust(((len(bBin) // 1024) + 1) * 1024, '0')
+
+    bBin = reverse(bBin, -306, -604)
+    bBin = lShift(bBin, 1024)
+    bBin = reverse(bBin, -910, -295)
+
+    finalBlock = bBin[:1024]
+    final = hex(int(finalBlock, 2))
+
+    return final
 
 @app.route('/hash', methods=['POST'])
 def hash_input():
     data = request.get_json()
-    if not data or 'text' not in data:
-        return jsonify({'error': 'Missing text field'}), 400
-
-    raw_text = data['text']
-    clean_text = sanitize_input(raw_text)
-    hashed = ibn_omar_hash(clean_text)
-
-    with open('hash_log.txt', 'a') as log:
-        log.write("input: {}\n".format(clean_text))
-        log.write("hash : {}\n".format(hashed))
-        log.write("-----\n")
-
-    return jsonify({'hash': hashed})
-
+    clean_text = data.get('text', '')
+    try:
+        hashed = ibn_omar_hash(clean_text)
+        return jsonify({'hash': hashed})
+    except Exception as e:
+        return jsonify({'hash': 'HASH_ERROR', 'error': str(e)}), 500
 
 if __name__ == '__main__':
     import os
