c5fc64ad934ec1df7ba902f5a874afc69887645e
RaedShafei <69829680+RaedShafei@users.noreply.github.com>
2025-06-07 13:35:07 +0300

    Update app_with_ibn_omar.py

--- app_with_ibn_omar.py
+++ app_with_ibn_omar.py
@@ -148,6 +148,7 @@ def hash_input():
 
     return jsonify({'hash': hashed})
 
+
 if __name__ == '__main__':
-import os
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
